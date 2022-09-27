cf68dc6231e03c0e3adece535aee7b7be6dbab6d
MichalSoszkoGID <97670480+MichalSoszkoGID@users.noreply.github.com>
2022-09-27 13:17:42 +0200

    Add rest of the resources

---
+++ Session 2 - hands-on solutions/models/intermediate/marketing/int_order_items_sale_pivoted.sql
@@ -0,0 +1,25 @@
+{{
+    config(
+      materialized='table',
+      persist_docs={"relation":true, "columns": true}
+    )
+}}
+
+with order_items as (
+    select * from {{ ref('stg_ecommerce__order_items')}}
+),
+pivot_order_items_agg_by_user as (
+    
+    select 
+        
+        user_id,
+        sum(case when order_item_status = 'Complete' then order_item_sale_price else 0 end)     as CLV,
+        count(distinct order_id)                                                                as order_cnt,
+        min(order_item_created_at)                                                              as first_order_date,
+        max(order_item_created_at)                                                              as most_recent_order_date
+
+    from order_items
+    group by user_id
+)
+
+select * from pivot_order_items_agg_by_user
